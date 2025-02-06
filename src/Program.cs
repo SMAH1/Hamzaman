d8a1895e0b833843b9f00ea50763185f8198da21
SMAH1 <integral22003@yahoo.com>
2025-02-06 17:45:58 +0330

    Get command line '--config' for overwrite 'appsettings.json'

--- src/Program.cs
+++ src/Program.cs
@@ -1,85 +1,157 @@
 using Hamzaman;
 using Microsoft.AspNetCore.SignalR;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateSlimBuilder(args);
-
-builder.Services.AddSignalR();
-builder.Services.Configure<JsonHubProtocolOptions>(o =>
+static class Program
 {
-    o.PayloadSerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-});
-
-// HTTPS  -----------------------------------------------------------
-builder.WebHost.UseKestrel(options =>
-{
-    options.ConfigureHttpsDefaults(httpsOptions =>
+    public enum ErrorMainReturn : int
     {
-    });
-});
+        OK = 0,
 
-// Application Settings  --------------------------------------------
-AppSettings appSettings = new AppSettings();
-var configuration = builder.Configuration.GetSection("AppSettings");
-builder.Services.Configure<AppSettings>(configuration);
-configuration.Bind(appSettings);
+        ErrorConfigFileNotExists = 1,
+        ErrorJsonFormatOfConfigFile = 2,
+        ErrorReadConfigFile = 3,
+    }
 
-// Embedded Files  --------------------------------------------------
-var embeddedFiles = new EmbeddedFiles();
+    [STAThread]
+    static int Main(string[] args)
+    {
+        ErrorMainReturn ret = ErrorMainReturn.OK;
+        string? appSettingFile = null;
 
-// CORS  ------------------------------------------------------------
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(
-        policy =>
+        (ret, appSettingFile) = GetSettingFile(args);
+        if (ret != ErrorMainReturn.OK) return (int)ret;
+
+        if (appSettingFile is not null)
         {
-            policy.WithOrigins($"http://{appSettings.Host}:{appSettings.HttpPort}");
-            if (appSettings.HttpsPort > 0 && appSettings.HttpsPort != appSettings.HttpPort)
-                policy.WithOrigins($"https://{appSettings.Host}:{appSettings.HttpsPort}");
+            ret = CheckJsonFile(appSettingFile);
+            if (ret != ErrorMainReturn.OK) return (int)ret;
+        }
 
-            foreach (var cors in appSettings.CORS)
-                policy.WithOrigins(cors);
+        Initialize(appSettingFile);
 
-            policy.AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials()
-            ;
+        return 0;
+    }
+
+    static (ErrorMainReturn, string?) GetSettingFile(string[] args)
+    {
+        string? appSettingFile = null;
+        if (args.Length >= 2 && string.Compare(args[0], "--config", true) == 0)
+        {
+            appSettingFile = args[1];
+            if (!File.Exists(appSettingFile))
+            {
+                Console.Error.WriteLine($"File '{appSettingFile}' is not exists.");
+                return (ErrorMainReturn.ErrorConfigFileNotExists, null);
+            }
+        }
+        return (ErrorMainReturn.OK, appSettingFile);
+    }
+
+    static ErrorMainReturn CheckJsonFile(string appSettingFile)
+    {
+        try
+        {
+            using (JsonDocument doc = JsonDocument.Parse(File.ReadAllText(appSettingFile))) { }
+        }
+        catch (JsonException)
+        {
+            Console.Error.WriteLine($"File '{appSettingFile}' is not JSON format.");
+            return ErrorMainReturn.ErrorJsonFormatOfConfigFile;
+        }
+        catch
+        {
+            Console.Error.WriteLine($"Uknown error in read file '{appSettingFile}'.");
+            return ErrorMainReturn.ErrorReadConfigFile;
+        }
+        return ErrorMainReturn.OK;
+    }
+
+    static void Initialize(string? appSettingFile)
+    {
+        var builder = WebApplication.CreateSlimBuilder();
+
+        builder.Services.AddSignalR();
+        builder.Services.Configure<JsonHubProtocolOptions>(o =>
+        {
+            o.PayloadSerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
         });
-});
 
-// Singaleton -------------------------------------------------------
-builder.Services.AddSingleton(appSettings);
-builder.Services.AddSingleton(embeddedFiles);
+        // HTTPS  -----------------------------------------------------------
+        builder.WebHost.UseKestrel(options =>
+        {
+            options.ConfigureHttpsDefaults(httpsOptions =>
+            {
+            });
+        });
 
-// APP  -------------------------------------------------------------
-var app = builder.Build();
+        // Application Settings  --------------------------------------------
+        AppSettings appSettings = new AppSettings();
+        if (appSettingFile is not null)
+            builder.Configuration.AddJsonFile(appSettingFile, true, false);
+        var configuration = builder.Configuration.GetSection("AppSettings");
+        builder.Services.Configure<AppSettings>(configuration);
+        configuration.Bind(appSettings);
 
-app.Urls.Add($"http://{appSettings.Host}:{appSettings.HttpPort}");
-if (appSettings.HttpsPort > 0 && appSettings.HttpsPort != appSettings.HttpPort)
-    app.Urls.Add($"https://{appSettings.Host}:{appSettings.HttpsPort}");
-else
-    Console.WriteLine("HTTPS disabled!");
+        // Embedded Files  --------------------------------------------------
+        var embeddedFiles = new EmbeddedFiles();
 
-app.UseCors();
+        // CORS  ------------------------------------------------------------
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(
+                policy =>
+                {
+                    policy.WithOrigins($"http://{appSettings.Host}:{appSettings.HttpPort}");
+                    if (appSettings.HttpsPort > 0 && appSettings.HttpsPort != appSettings.HttpPort)
+                        policy.WithOrigins($"https://{appSettings.Host}:{appSettings.HttpsPort}");
 
-if (appSettings.Message.Enable && !string.IsNullOrEmpty(appSettings.Message.Hub))
-{
-    app.MapHub<MessageHub>("/" + appSettings.Message.Hub);
+                    foreach (var cors in appSettings.CORS)
+                        policy.WithOrigins(cors);
+
+                    policy.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                    ;
+                });
+        });
+
+        // Singaleton -------------------------------------------------------
+        builder.Services.AddSingleton(appSettings);
+        builder.Services.AddSingleton(embeddedFiles);
+
+        // APP  -------------------------------------------------------------
+        var app = builder.Build();
+
+        app.Urls.Add($"http://{appSettings.Host}:{appSettings.HttpPort}");
+        if (appSettings.HttpsPort > 0 && appSettings.HttpsPort != appSettings.HttpPort)
+            app.Urls.Add($"https://{appSettings.Host}:{appSettings.HttpsPort}");
+        else
+            Console.WriteLine("HTTPS disabled!");
+
+        app.UseCors();
+
+        if (appSettings.Message.Enable && !string.IsNullOrEmpty(appSettings.Message.Hub))
+        {
+            app.MapHub<MessageHub>("/" + appSettings.Message.Hub);
+        }
+        else
+            Console.WriteLine("Message Hub disabled!");
+
+        if (appSettings.Server.Enable && !string.IsNullOrEmpty(appSettings.Server.Hub))
+        {
+            app.MapHub<ServerHub>("/" + appSettings.Server.Hub);
+        }
+        else
+            Console.WriteLine("Server Hub disabled!");
+
+        app.MapGet("/", () => TypedResults.Redirect("/index.html"));
+        app.StaticFilesApi(appSettings, embeddedFiles);
+
+        app.Run();
+    }
 }
-else
-    Console.WriteLine("Message Hub disabled!");
-
-if (appSettings.Server.Enable && !string.IsNullOrEmpty(appSettings.Server.Hub))
-{
-    app.MapHub<ServerHub>("/" + appSettings.Server.Hub);
-}
-else
-    Console.WriteLine("Server Hub disabled!");
-
-app.MapGet("/", () => TypedResults.Redirect("/index.html"));
-app.StaticFilesApi(appSettings, embeddedFiles);
-
-app.Run();
 
 [JsonSerializable(typeof(string))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext { }
